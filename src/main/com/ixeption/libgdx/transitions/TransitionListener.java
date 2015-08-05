afb47bee4ca13d1abc1967efd13ab49538dbb385
iXeption <iniquitous1988@gmail.com>
2015-08-05 13:57:55 +0200

    -initial

---
+++ src/main/com/ixeption/libgdx/transitions/TransitionListener.java
@@ -0,0 +1,13 @@
+package com.ixeption.libgdx.transitions;
+
+/**
+ * A listener for transition events
+ * @author iXeption
+ *
+ */
+public interface TransitionListener {
+	
+	void onTransitionStart();
+	void onTransitionFinished();
+
+}
